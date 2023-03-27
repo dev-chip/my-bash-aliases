8c77dce8ffed3bae4d18fc2adc5100f49385e43b
James Cook <62399579+dev-chip@users.noreply.github.com>
2023-03-27 01:02:59 +0100

    Create grab.sh

---
+++ grab.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set +e
+cp -fv ~/.bash_profile .
+cp -fv ~/.bashrc .
+cp -fv ~/.bash_aliases .
+cp -fv ~/.tmux.conf .
+cp -fv ~/.vimrc .
+cp -fv ~/.gitconfig .
+cp -fv ~/.git-templates/hooks/prepare-commit-msg .
+echo "grab successful"
